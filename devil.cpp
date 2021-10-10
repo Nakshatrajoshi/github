25d767ccc6d56391a7ddfc47cdd922d3d9506417
Nakshatra <nakshatrajoshi999@gmail.com>
2021-10-10 15:23:32 +0530

    skipped staging area

---
+++ devil.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std ;
+
+int main(){
+int a ;
+int b ;
+cout <<" enter the numbr a ";
+cin >> a ;
+cout << " enter the number b ";
+cin >> b ;
+int c = a + b;
+cout << c ;
+return 0;
+}
